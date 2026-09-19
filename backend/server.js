1f373ca9947268760c0bd567f7987c1813b8cb0c
Ktaro <justinkentejada@gmail.com>
2026-09-19 11:08:02 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -122,7 +122,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB connected.");
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
